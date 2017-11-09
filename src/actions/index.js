a0995c4b198d353e71d38d29cb010a062d95700f
Just RAG <justrag@gmail.com>
2017-11-09 19:35:09 +0100

    unblock reducer correction

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,34 +5,34 @@ import { createAction } from 'redux-act';
 // 'roll' for roll
 
 export const startGame = createAction('START_GAME', null, () => ({
-	sound: `start`,
-	roll: true
+  sound: `start`,
+  roll: true
 }));
 export const startHelp = createAction('START_HELP', null, () => ({
-	sound: `start`
+  sound: `start`
 }));
 export const endGame = createAction('END_GAME', null, payload => ({
-	sound: payload ? 'success' : 'failure'
+  sound: payload ? 'success' : 'failure'
 }));
 export const chooseDuck = createAction(
-	'CHOOSE_DUCK',
-	(duckId, roll) => ({ duckId, roll }),
-	duckId => ({ sound: `duck${duckId}`, roll: true })
+  'CHOOSE_DUCK',
+  (duckId, roll) => ({ duckId, roll }),
+  duckId => ({ sound: `duck${duckId}`, roll: true })
 );
 export const unblock = createAction(
-	'UNBLOCK',
-	() => ({ bulba: true }),
-	() => ({
-		sound: `duck3`,
-		roll: true
-	})
+  'UNBLOCK',
+  duckId => ({ duckId }),
+  () => ({
+    sound: `duck3`,
+    roll: true
+  })
 );
 export const pause = createAction('PAUSE', null, () => ({ sound: `duck3` }));
 export const stage = createAction('STAGE', null, () => ({ sound: `duck1` }));
 
 export const continueGame = createAction('CONTINUE_GAME', null, () => ({
-	sound: `duck3`
+  sound: `duck3`
 }));
 export const forfeitGame = createAction('FORFEIT_GAME', null, payload => ({
-	sound: 'failure'
+  sound: 'failure'
 }));
--- src/reducers/ducks.js
+++ src/reducers/ducks.js
@@ -2,33 +2,50 @@ import { createReducer } from 'redux-act';
 import R from 'ramda';
 import { startGame, chooseDuck, unblock } from '../actions/';
 
-const duckIds = R.range(1,7); //[1, 2, 3, 4, 5, 6]
+const duckIds = R.range(1, 7); //[1, 2, 3, 4, 5, 6]
 
-const pondIds = R.range(0,7); // [0, 1, 2, 3, 4, 5, 6]
+const pondIds = R.range(0, 7); // [0, 1, 2, 3, 4, 5, 6]
 
 //export const getDucks = (state) => duckIds.map(id => state[id]);
-export const getDucks = (state) => duckIds.map(id => ({...state[id], seq: duckIds.filter(d => (state[d].position === state[id].position && id > d)).length}));
-export const getPonds = (state) => pondIds.map(id => ({id, ducks: getDucks(state).filter(d => (d.position === id))}));
-export const isWon = (state) => getDucks(state).filter(d => (d.position === 0)).length === 6;
-export const isFirstPondEmpty = (state) => (getDucks(state).filter(d => (d.position === 0)).length === 0);
+export const getDucks = state =>
+  duckIds.map(id => ({
+    ...state[id],
+    seq: duckIds.filter(d => state[d].position === state[id].position && id > d)
+      .length
+  }));
+export const getPonds = state =>
+  pondIds.map(id => ({
+    id,
+    ducks: getDucks(state).filter(d => d.position === id)
+  }));
+export const isWon = state =>
+  getDucks(state).filter(d => d.position === 0).length === 6;
+export const isFirstPondEmpty = state =>
+  getDucks(state).filter(d => d.position === 0).length === 0;
 
 // 1: {id: 1, position: 6},
 // 2: {id: 2, position: 6},
 // ...
 // 6: {id: 6, position: 6},
-const startingDucksReducer = () => R.indexBy(R.prop('id'), R.map(id => ({id, position: 6}), duckIds));
+const startingDucksReducer = () =>
+  R.indexBy(R.prop('id'), R.map(id => ({ id, position: 6 }), duckIds));
 
-const moveDuckToPosition = (ducks, duckId, position) => R.assocPath([duckId, 'position'], position, ducks);
+const moveDuckToPosition = (ducks, duckId, position) =>
+  R.assocPath([duckId, 'position'], position, ducks);
 
-const chooseDuckReducer = (state, { duckId, roll } ) => moveDuckToPosition(state, duckId, state[duckId].position - roll);
+const chooseDuckReducer = (state, { duckId, roll }) =>
+  moveDuckToPosition(state, duckId, state[duckId].position - roll);
 
-const unblockReducer = (state, duckId) => moveDuckToPosition(state, duckId, 6);
+const unblockReducer = (state, { duckId }) =>
+  moveDuckToPosition(state, duckId, 6);
 
-const ducks = createReducer({
-  [startGame]: startingDucksReducer,
-  [chooseDuck]: chooseDuckReducer,
-  [unblock]: unblockReducer,
-}, {}
-  );
+const ducks = createReducer(
+  {
+    [startGame]: startingDucksReducer,
+    [chooseDuck]: chooseDuckReducer,
+    [unblock]: unblockReducer
+  },
+  {}
+);
 
 export default ducks;
